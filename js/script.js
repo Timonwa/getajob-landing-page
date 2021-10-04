2351cc83cd2fe7178a73656ec225bfd9fafdfcb1
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2021-10-04 12:41:03 +0100

    made the how it works section further responsive.

--- js/script.js
+++ js/script.js
@@ -32,13 +32,13 @@ menu.addEventListener("click", function () {
 var next = document.querySelector('.next-btn>i');
 next.onclick = function () {
     var container = document.querySelector('.job-cards-cntr');
-    sideScroll(container, 'right', 25, 100, 10);
+    sideScroll(container, 'right', 20, 340, 10);
 };
 
 var prev = document.querySelector('.prev-btn>i');
 prev.onclick = function () {
     var container = document.querySelector('.job-cards-cntr');
-    sideScroll(container, 'left', 25, 100, 10);
+    sideScroll(container, 'left', 25, 340, 10);
 };
 
 function sideScroll(element, direction, speed, distance, step) {
